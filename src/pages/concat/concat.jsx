5013435ec9d10b687f3aec0b4c0a2e0046fa1de3
shadow <wuh131420@foxmail.com>
2020-03-12 18:41:25 +0800

    feat(concat): 添加联系方式

--- src/pages/concat/concat.jsx
+++ src/pages/concat/concat.jsx
@@ -5,10 +5,25 @@ import wechatQrCode from './assets/wechat.jpeg'
 import './index.scss'
 import ListWrapper from '../../components/List'
 
-const maps = {
+const Maps = {
     qq: qqQrcode,
     wechat: wechatQrCode
 }
+
+const Links = [
+    {
+        label: 'QQ',
+        value: '724132485@qq.com'
+    },
+    {
+        label: 'Foxmail',
+        value: 'wuh131420@foxmail.com'
+    },
+    {
+        label: '163',
+        value: 'wuh_201314@163.com'
+    }
+]
 const NormalConcat = () => {
     const router = useRouter()
     const { params: { mode = 'qq' } } = router
@@ -17,18 +32,20 @@ const NormalConcat = () => {
             title: mode + '联系我吧'
         })
     }, [mode])
+    const handleClip = (str) => {
+        Taro.setClipboardData({data: str})
+    }
     if (mode === 'email') {
         return (<View className='concat'>
-            <ListWrapper>
-                <Text>QQ: 724132485@qq.com</Text>
-            </ListWrapper>
-            <ListWrapper>
-                <Text>Foxmail: wuh131420@foxmail.com</Text>
-            </ListWrapper>
+            {
+                Links.map(v => (<ListWrapper key={v.label}>
+                    <Text onClick={() => handleClip(v.value)}>{v.label}: {v.value}</Text>
+                </ListWrapper>))
+            }
         </View>)
     }
     return (<View className='concat'>
-        <Image mode='widthFix' src={maps[mode]}></Image>
+        <Image mode='widthFix' src={Maps[mode]}></Image>
     </View>)
 }
 
