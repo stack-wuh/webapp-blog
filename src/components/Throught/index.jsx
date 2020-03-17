46d06ce81bcfef4c270f58e488cb3f5ce7f8e05b
shadow <wuh131420@foxmail.com>
2020-03-17 11:47:42 +0800

    feat(Throught): 添加border的Visible

--- src/components/Throught/index.jsx
+++ src/components/Throught/index.jsx
@@ -1,9 +1,10 @@
 import Taro from '@tarojs/taro'
 import {View, Text} from '@tarojs/components'
 import propTypes from 'prop-types'
+import classnames from 'classnames'
 import './index.scss'
 
-const Throught = ({text, pos, width}) => {
+const Throught = ({text, pos, width, border}) => {
     const position = {
         center: 0,
         left: -1,
@@ -16,21 +17,23 @@ const Throught = ({text, pos, width}) => {
         width
     }
     return (<View className='throught' style={outerStyle}>
-        <Text className='throught__line'></Text>
+        <Text className={classnames('throught__line', {'is-border': !border})} ></Text>
         <Text className='throught__text' style={style}>{text}</Text>
-        <Text className='throught__line'></Text>
+        <Text className={classnames('throught__line', {'is-border': !border})}></Text>
     </View>)
 }
 
 Throught.propTypes = {
     text: propTypes.string.isRequired,
     pos: propTypes.string,
-    width: propTypes.string
+    width: propTypes.string,
+    border: propTypes.bool
 }
 Throught.defaultProps = {
     text: '',
     pos: 'left',
-    width: '100%'
+    width: '100%',
+    border: true
 }
 
 export default Throught
